b01b7d4f8a08fa56903132fd5c6b6667414cdca4
Sergey Mezentsev <thebits@yandex.ru>
2011-12-16 02:03:12 +0400

    Улучшены сообщения;Добавлена проверка закрытия файла

--- linux/mbftdi.c
+++ linux/mbftdi.c
@@ -1,7 +1,9 @@
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <errno.h>
 #include <string.h>
+
 #include "../common/ftd2xx.h"
 
 #define false 0
@@ -17,23 +19,28 @@ int main(int argc, char* argv[])
 	printf("FTDI port to JTAG is used for programming\n");
 	printf("Usage example: >mbftdi myfile.svf\n\n");
 
-	if(argc<2)
+	if (argc < 2)
 	{
-		printf("No input parameters!\n");
-		return -1;
+		fprintf(stderr, "No input parameters!\n");
+		exit(EXIT_FAILURE);
 	}
 
 	//open file
-	FILE* f = fopen( argv[1],"r");
+	FILE* f = fopen(argv[1], "r");
 
-	if(f==NULL)
+	if (f == NULL)
 	{
-		printf("Cannot open SVF file %s\n",argv[2]);
-		return -1;
+		fprintf(stderr, "Cannot open SVF file %s: %s\n", argv[1], strerror(errno));
+		exit(EXIT_FAILURE);
 	}
 
 	play_svf(f);
 
-	fclose(f);
-	return 0;
+	int ret = fclose(f);
+	if (ret != 0)
+	{
+		fprintf(stderr, "The SVF file was not closed normally: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	exit(EXIT_SUCCESS);
 }
